52975c01bf75c5a719932380459064debf01de3b
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-10-13 12:44:04 -0600

    adding in components

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="row">
+      <div className="col">
+        <nav className="navbar navbar-default">
+          <div className="navbar-header">
+            <a className="navbar-brand">React Inbox</a>
+          </div>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
